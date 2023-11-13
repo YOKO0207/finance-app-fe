304a897f61cd85613534259edd8fb0ad24de9ced
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-11-13 21:34:13 +0900

    create note edit screen

---
+++ src/components/screens/NoteEditScreen.tsx
@@ -0,0 +1,103 @@
+import { useNoteDetailSWR, useNoteUpdateHandler } from "@/hooks";
+import { NoteUpdateInput } from "@/types";
+import { StatusBar } from "expo-status-bar";
+import { Formik } from "formik";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import * as Yup from "yup";
+
+interface Props {
+	route: any;
+}
+export const NoteEditScreen = ({ route }: Props) => {
+	const { noteId } = route.params;
+	const { data: note } = useNoteDetailSWR({ noteId });
+
+	const { handleNoteUpdate } = useNoteUpdateHandler();
+
+	const validationSchema = Yup.object().shape({
+		note_title: Yup.string().required("タイトルは必須です"),
+		person_name: Yup.string().required("相手の名前は必須です"),
+	});
+
+	const initialValues = {
+		note_title: note?.data?.data?.note_title || "",
+		person_name: note?.data?.data?.note_title || "",
+	};
+
+	const handleFormSubmit = (input: NoteUpdateInput) => {
+		const validationErrors = handleNoteUpdate({ input, noteId });
+		return validationErrors;
+	};
+
+	return (
+		<View>
+			<StatusBar style="auto" />
+			<Text>{note?.data?.data?.note_title}</Text>
+			<Formik
+				initialValues={initialValues}
+				validationSchema={validationSchema}
+				onSubmit={handleFormSubmit}
+			>
+				{({
+					handleChange,
+					handleBlur,
+					handleSubmit,
+					values,
+					errors,
+					touched,
+				}) => (
+					<>
+						<View style={styles.inputContainer}>
+							<Text>Note Title</Text>
+							<TextInput
+								style={styles.input}
+								onChangeText={handleChange("note_title")}
+								onBlur={handleBlur("note_title")}
+								value={values.note_title}
+							/>
+							{touched.note_title && errors.note_title && (
+								<Text style={styles.error}>{errors.note_title}</Text>
+							)}
+						</View>
+
+						<View style={styles.inputContainer}>
+							<Text>Person Name</Text>
+							<TextInput
+								style={styles.input}
+								onChangeText={handleChange("person_name")}
+								onBlur={handleBlur("person_name")}
+								value={values.person_name}
+							/>
+							{touched.person_name && errors.person_name && (
+								<Text style={styles.error}>{errors.person_name}</Text>
+							)}
+						</View>
+
+						<Button onPress={() => handleSubmit()} title="Submit" />
+					</>
+				)}
+			</Formik>
+			<Text>{noteId}</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		padding: 20,
+	},
+	inputContainer: {
+		marginBottom: 20,
+	},
+	input: {
+		borderWidth: 1,
+		borderColor: "gray",
+		padding: 10,
+		borderRadius: 4,
+	},
+	error: {
+		color: "red",
+	},
+});
--- src/components/screens/NoteIndexScreen.tsx
+++ src/components/screens/NoteIndexScreen.tsx
@@ -3,11 +3,14 @@ import { View, Alert } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { Button, Text } from "native-base";
 import { showMessage } from "react-native-flash-message";
+import { useNoteIndexSWR, useNoteDeleteHandler } from "@/hooks";
+import { Notes } from "@/types";
 
 type RootStackParamList = {
-	NoteIndexScreen: undefined; 
-	NoteNewScreen: undefined; 
+	NoteIndexScreen: undefined;
+	NoteNewScreen: undefined;
 	TransactionIndexScreen: undefined;
+	NoteEditScreen: { noteId: string};
 };
 type NoteIndexScreenProps = StackNavigationProp<
 	RootStackParamList,
@@ -19,6 +22,10 @@ interface Props {
 export const NoteIndexScreen = (props: Props) => {
 	const { navigation } = props;
 
+	const { data: notes } = useNoteIndexSWR();
+
+	const { handleNoteDelete } = useNoteDeleteHandler();
+
 	// const showErrorAlert = (errorMessage: string) => {
 	// 	Alert.alert("Error", errorMessage, [{ text: "OK" }], { cancelable: false });
 	// };
@@ -29,13 +36,19 @@ export const NoteIndexScreen = (props: Props) => {
 			<Button onPress={() => navigation.navigate("NoteNewScreen")}>
 				go to note new
 			</Button>
-			<Button onPress={() => {
-				showMessage({
-					message: "Hello World",
-					type: "info",
-				});
-			}}>show error</Button>
-			<Text onPress={() => navigation.navigate("TransactionIndexScreen")}>取引一覧</Text>
+			{notes?.data?.data?.map((item: Notes) => (
+				<>
+					<Text key={item.id}>{item.note_title}</Text>
+					<Text onPress={() => navigation.navigate("NoteEditScreen", { noteId: item.id})}>
+						編集
+					</Text>
+					<Button onPress={() => handleNoteDelete({noteId: item.id})}>削除</Button>
+					{/* <Text onPress={() => navigation.navigate("TransactionIndexScreen")}>
+						取引一覧
+					</Text> */}
+				</>
+			))}
+			
 		</View>
 	);
 };
