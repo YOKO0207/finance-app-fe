91c4e00b96ffa00c2c0d8f29397d65732ff9d4d8
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-11-13 14:41:51 +0900

    add backend api urls

---
+++ src/constants/backendApiUrls.ts
@@ -0,0 +1,10 @@
+export const BACKEND_API_URLS = {
+	NOTES: {
+		NOTES: "notes",
+		NOTE: "note/:noteId",
+	},
+	TRANSACTIONS: {
+		TRANSACTIONS: "notes/:noteId/transactions",
+		TRANSACTION: "notes/:noteId/transaction/:transactionId",
+	},
+};
