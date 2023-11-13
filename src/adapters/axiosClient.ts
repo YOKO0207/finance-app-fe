206c51e59a36a0e349e63ecb0445c15f09f6a58f
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-11-13 14:41:02 +0900

    modify axios client

--- src/adapters/axiosClient.ts
+++ src/adapters/axiosClient.ts
@@ -1,7 +1,6 @@
-import { BASE_FIREBSE_BACKEND_URL } from "@env";
 import axios from "axios";
 
-const baseURL = BASE_FIREBSE_BACKEND_URL;
+const baseURL = process.env.EXPO_PUBLIC_BASE_FIREBSE_BACKEND_URL || "";
 const jsonHeaders = { "Content-Type": "application/json" };
 
 const createAxiosClient = (baseURL: string, headers: any) => {
@@ -15,7 +14,6 @@ export const axiosClient = createAxiosClient(baseURL, jsonHeaders);
 
 export const setAuthToken = (token: string | null) => {
 	if (token) {
-		
 		axiosClient.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 	} else {
 		delete axiosClient.defaults.headers.common["Authorization"];
