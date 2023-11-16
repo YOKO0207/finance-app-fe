5c26b788d9b2fe4ed8be1d7877e7c07c4016d2db
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-11-16 17:01:53 +0900

    create note card

---
+++ src/components/organisms/cards/NoteCard.tsx
@@ -0,0 +1,132 @@
+import { CURRENCIES } from "@/constants";
+import { colors, shadows } from "@/styles";
+import { Note } from "@/types";
+import { Box } from "native-base";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SheetManager } from "react-native-actions-sheet";
+import Icon from "react-native-vector-icons/AntDesign";
+import FeatherIcon from "react-native-vector-icons/Feather";
+
+interface Props {
+	note: Note | undefined;
+	onDeletePress?: () => void;
+	onEditPress?: () => void;
+}
+
+export const NoteCard = (props: Props) => {
+	const { note, onDeletePress, onEditPress } = props;
+
+	const onEditPressWrapper = () => {
+		if (onEditPress) {
+			onEditPress();
+			SheetManager.hide("card-action-sheet");
+		}
+	};
+
+	const onDeletePressWrapper = () => {
+		if (onDeletePress) {
+			onDeletePress();
+			SheetManager.hide("card-action-sheet");
+		}
+	}
+
+	return (
+		<View style={styles.container}>
+			<Box style={styles.header}>
+				<Text style={styles.headerTitle}>{note?.note_title || ""}</Text>
+				{onDeletePress && onEditPress && (
+					<TouchableOpacity
+						onPress={() => {
+							SheetManager.show("card-action-sheet", {
+								payload: {
+									onDeletePress: onDeletePressWrapper,
+									onEditPress: onEditPressWrapper,
+								},
+							});
+						}}
+					>
+						<FeatherIcon name="more-vertical" size={24} color={colors.gray[900]}/>
+					</TouchableOpacity>
+				)}
+				
+			</Box>
+			<Box>
+				<Text style={styles.totalTitle}>貸し借り金額</Text>
+				<Box style={styles.contentWrapper}>
+					<Text style={styles.totalText}>{`${
+						note?.currency_type && CURRENCIES[note?.currency_type] || ""
+					}${note?.total}`}</Text>
+					<Box style={styles.userWrapper}>
+						<Box style={styles.userIcon}>
+							<Icon name="user" size={18} color="white" />
+						</Box>
+						<Text style={styles.userText}>{note?.person_name || ""}</Text>
+					</Box>
+				</Box>
+			</Box>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: "white",
+		paddingHorizontal: 18,
+		paddingVertical: 20,
+		borderRadius: 8,
+		...shadows[10],
+	},
+	header: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+		borderBottomWidth: 1,
+		borderBottomColor: colors.gray[50],
+		paddingBottom: 18,
+		marginBottom: 18,
+	},
+	headerTitle: {
+		fontSize: 18,
+		fontWeight: "bold",
+		color: colors.gray[900],
+	},
+	contentWrapper: {
+		justifyContent: "space-between",
+		flexDirection: "row",
+	},
+	totalTitle: {
+		color: colors.gray[200],
+		fontWeight: "bold",
+		fontSize: 12,
+		marginBottom: 4,
+	},
+	totalText: {
+		color: colors.secondary[500],
+		fontWeight: "bold",
+		fontSize: 24,
+	},
+	userName: {
+		fontSize: 14,
+		fontWeight: "bold",
+		color: colors.gray[900],
+	},
+	userWrapper: {
+		flexDirection: "row",
+		alignItems: "center",
+		gap: 4,
+	},
+	userIcon: {
+		backgroundColor: colors.primary[500],
+		width: 28,
+		height: 28,
+		borderRadius: 8,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	userText: {
+		color: colors.gray[900],
+		fontSize: 14,
+		fontWeight: "bold",
+		marginBottom: 4
+	},
+});
