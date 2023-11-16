57e5358b4cb9b3cf5c27fc658e3d38dfdd20c6a2
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-11-16 17:01:29 +0900

    create action sheet

---
+++ src/components/organisms/actionSheets/CardActionSheet.tsx
@@ -0,0 +1,94 @@
+import ActionSheet, { SheetProps } from "react-native-actions-sheet";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/Feather";
+import { Box } from "native-base";
+import { colors } from "@/styles";
+import { ConfirmDialog } from "react-native-simple-dialogs";
+import { useState } from "react";
+
+interface Props {
+	onEditPress: () => void;
+	onDeletePress: () => void;
+}
+
+export const CardActionSheet = (props: SheetProps<Props>) => {
+	const { payload } = props;
+
+	const [dialogVisible, setDialogVisible] = useState<boolean>(false);
+
+	return (
+		<ActionSheet id={props.sheetId}>
+			<View style={styles.container}>
+				<TouchableOpacity
+					style={styles.actionButton}
+					onPress={payload?.onEditPress}
+				>
+					<Box style={styles.iconWrapper}>
+						<Icon name="edit-2" size={18} />
+					</Box>
+					<Text style={styles.actionText}>編集する</Text>
+				</TouchableOpacity>
+
+				<TouchableOpacity onPress={() => setDialogVisible(true)}>
+					<Box style={styles.actionButton}>
+						<Box style={{ ...styles.iconWrapper, ...styles.deleteIconWrapper }}>
+							<Icon name="delete" size={16} color="white" />
+						</Box>
+						<Text style={{ ...styles.actionText, ...styles.deleteText }}>
+							削除する
+						</Text>
+						<ConfirmDialog
+							title="確認画面"
+							message="本当に実行しますか？"
+							visible={dialogVisible}
+							onTouchOutside={() => setDialogVisible(false)}
+							positiveButton={{
+								title: "はい",
+								onPress: () => {
+									setDialogVisible(false);
+									payload?.onDeletePress();
+								},
+							}}
+							negativeButton={{
+								title: "いいえ",
+								onPress: () => setDialogVisible(false),
+							}}
+						/>
+					</Box>
+				</TouchableOpacity>
+			</View>
+		</ActionSheet>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		gap: 32,
+		paddingVertical: 32,
+		paddingHorizontal: 16,
+	},
+	actionButton: {
+		flexDirection: "row",
+		alignItems: "center",
+	},
+	actionText: {
+		fontSize: 16,
+		marginLeft: 8,
+		color: colors.gray[900],
+	},
+	deleteText: {
+		color: colors.secondary[500],
+	},
+	iconWrapper: {
+		alignItems: "center",
+		justifyContent: "center",
+		width: 34,
+		height: 34,
+		borderRadius: 40,
+		backgroundColor: colors.gray[100],
+	},
+	deleteIconWrapper: {
+		paddingRight: 2,
+		backgroundColor: colors.error[100],
+	},
+});
