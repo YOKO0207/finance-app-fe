42700b998eeeda671d87326a7e364b44a2696f46
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-11-16 17:02:22 +0900

    create transaction card

---
+++ src/components/organisms/cards/TransactionCard.tsx
@@ -0,0 +1,92 @@
+import { CURRENCIES } from "@/constants";
+import { colors, shadows } from "@/styles";
+import { Transactions } from "@/types";
+import { Box } from "native-base";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SheetManager } from "react-native-actions-sheet";
+import FeatherIcon from "react-native-vector-icons/Feather";
+
+interface Props {
+	transaction: Transactions;
+	onDeletePress: () => void;
+	onEditPress: () => void;
+}
+
+export const TransactionCard = (props: Props) => {
+	const { transaction, onDeletePress, onEditPress } = props;
+
+	const onEditPressWrapper = () => {
+		onEditPress();
+		SheetManager.hide("card-action-sheet");
+	};
+
+	const onDeletePressWrapper = () => {
+		onDeletePress();
+		SheetManager.hide("card-action-sheet");
+	};
+
+	return (
+		<View style={styles.container}>
+			<Box>
+				<Text style={styles.descriptionText}>
+					{transaction.transaction_desctiption || ""}
+				</Text>
+				<Text style={styles.dateText}>{transaction.created_at || ""}</Text>
+			</Box>
+			<Box style={styles.totalWrapper}>
+				<Text style={styles.totalText}>{`${
+					CURRENCIES[transaction.currency_type] || ""
+				}${transaction.amount}`}</Text>
+				<TouchableOpacity
+					onPress={() => {
+						SheetManager.show("card-action-sheet", {
+							payload: {
+								onDeletePress: onDeletePressWrapper,
+								onEditPress: onEditPressWrapper,
+							},
+						});
+					}}
+				>
+					<FeatherIcon
+						name="more-vertical"
+						size={20}
+						color={colors.gray[900]}
+					/>
+				</TouchableOpacity>
+			</Box>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: "white",
+		paddingHorizontal: 18,
+		paddingVertical: 20,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		aluignItems: "center",
+		borderBottomWidth: 1,
+		borderColor: colors.gray[50],
+		...shadows[10],
+	},
+	descriptionText: {
+		fontSize: 14,
+		color: colors.gray[900],
+		fontWeight: "bold",
+		marginBottom: 4,
+	},
+	dateText: {
+		fontSize: 12,
+		color: colors.gray[200],
+	},
+	totalText: {
+		color: colors.secondary[500],
+		fontSize: 18,
+	},
+	totalWrapper: {
+		flexDirection: "row",
+		alignItems: "center",
+		gap: 4
+	}
+});
