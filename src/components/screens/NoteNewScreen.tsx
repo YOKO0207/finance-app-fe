d9810e6c54cae634c444963f2d422895a56ff60c
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-11-17 13:51:08 +0900

    modify note new screen

--- src/components/screens/NoteNewScreen.tsx
+++ src/components/screens/NoteNewScreen.tsx
@@ -1,124 +1,24 @@
+import { AppLayoutA } from "@/components/layouts";
+import { NoteForm } from "@/components/organisms";
 import { useNoteCreateHandler } from "@/hooks";
-import { NoteCreateInput } from "@/types";
-import { StatusBar } from "expo-status-bar";
-import {
-	View,
-	TextInput,
-	Button,
-	Text,
-	StyleSheet
-} from "react-native";
-import { Picker } from "@react-native-picker/picker";
-import * as Yup from "yup";
-import { Formik } from "formik";
-import { CURRENCIES } from "@/constants";
+import { NoteInput } from "@/types";
 
 export const NoteNewScreen = () => {
 	const { handleNoteCreate } = useNoteCreateHandler();
 
-	const validationSchema = Yup.object().shape({
-		note_title: Yup.string().required("タイトルは必須です"),
-		person_name: Yup.string().required("相手の名前は必須です"),
-		currency_type: Yup.string().required("通貨を選択してください"),
-	});
-
-	const initialValues = {
+	const initialValues: NoteInput = {
 		note_title: "",
 		person_name: "",
-		currency_type: "",
+		currency_type: "JPY",
 	};
-
-	const handleFormSubmit = (input: NoteCreateInput) => {
-		const validationErrors = handleNoteCreate({ input });
-		return validationErrors;
-	};
+	
+		const handleFormSubmit = (input: NoteInput) => {
+			handleNoteCreate({ input });
+		};
 
 	return (
-		<View>
-			<StatusBar style="auto" />
-			<Formik
-				initialValues={initialValues}
-				validationSchema={validationSchema}
-				onSubmit={handleFormSubmit}
-			>
-				{({
-					handleChange,
-					handleBlur,
-					handleSubmit,
-					setFieldValue,
-					values,
-					errors,
-					touched,
-				}) => (
-					<>
-						<View style={styles.inputContainer}>
-							<Text>Note Title</Text>
-							<TextInput
-								style={styles.input}
-								onChangeText={handleChange("note_title")}
-								onBlur={handleBlur("note_title")}
-								value={values.note_title}
-							/>
-							{touched.note_title && errors.note_title && (
-								<Text style={styles.error}>{errors.note_title}</Text>
-							)}
-						</View>
-
-						<View style={styles.inputContainer}>
-							<Text>Person Name</Text>
-							<TextInput
-								style={styles.input}
-								onChangeText={handleChange("person_name")}
-								onBlur={handleBlur("person_name")}
-								value={values.person_name}
-							/>
-							{touched.person_name && errors.person_name && (
-								<Text style={styles.error}>{errors.person_name}</Text>
-							)}
-						</View>
-
-						<View style={styles.inputContainer}>
-							<Text>Select a Currency:</Text>
-							<Picker
-								selectedValue={values.currency_type}
-								onValueChange={(itemValue) =>
-									setFieldValue("currency_type", itemValue)
-								}
-							>
-								{Object.entries(CURRENCIES).map(([code, symbol]) => (
-									<Picker.Item
-										key={code}
-										label={`${code} (${symbol})`}
-										value={code}
-									/>
-								))}
-							</Picker>
-						</View>
-
-						<Button onPress={() => handleSubmit()} title="Submit" />
-					</>
-				)}
-			</Formik>
-		</View>
+		<AppLayoutA>
+			<NoteForm onSubmit={handleFormSubmit} initialValues={initialValues} />
+		</AppLayoutA>
 	);
 };
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		padding: 20,
-	},
-	inputContainer: {
-		marginBottom: 20,
-	},
-	input: {
-		borderWidth: 1,
-		borderColor: "gray",
-		padding: 10,
-		borderRadius: 4,
-	},
-	error: {
-		color: "red",
-	},
-});
