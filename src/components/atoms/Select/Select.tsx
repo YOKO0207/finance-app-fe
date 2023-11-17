140571d04cb04eb05fe911e3bf2af2ef8b1b1247
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-11-17 13:49:04 +0900

    create select component

---
+++ src/components/atoms/Select/Select.tsx
@@ -0,0 +1,54 @@
+import { Select as NativeBaseSelect } from "native-base";
+import { inputStyles } from "@/components/atoms/styles";
+import { Box } from "native-base";
+import { View, Text } from "react-native";
+import { colors } from "@/styles";
+
+interface Props {
+	selectedValue: string;
+	onValueChange: (itemValue: string) => void;
+	items: { label: string; value: string }[];
+	label: string;
+	note?: string[]
+	error?: string;
+}
+
+export const Select = (props: Props) => {
+	const { selectedValue, onValueChange, items, label, note, error } = props;
+	return (
+		<View style={inputStyles.inputContainer}>
+			<Box style={inputStyles.textWrapper}>
+				<Text style={inputStyles.lable}>{label}</Text>
+				{note && (
+					<Box style={inputStyles.noteWrapper}>
+						{note.map((item: string) => (
+							<Text style={inputStyles.note}>{item}</Text>
+						))}
+					</Box>
+				)}
+			</Box>
+			<NativeBaseSelect
+				selectedValue={selectedValue}
+				accessibilityLabel="選択してください"
+				placeholder="選択してください"
+				onValueChange={onValueChange}
+				_selectedItem={{
+					bg: "gray.300",
+				}}
+				backgroundColor={"white"}
+				borderColor={colors.gray[40]}
+				py={3}
+				borderRadius={4}
+			>
+				{items.map((item, index) => (
+					<NativeBaseSelect.Item
+						key={index}
+						label={item.label}
+						value={item.value}
+					/>
+				))}
+			</NativeBaseSelect>
+			{error && <Text style={inputStyles.error}>{error}</Text>}
+		</View>
+	);
+}
